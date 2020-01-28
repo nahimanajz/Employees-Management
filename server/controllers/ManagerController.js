4719b52da2895c8811c9f37d1c791f26f4db3858
Nahimana <na-janvier@bw-africa.com>
2020-01-28 11:52:45 +0200

    If applied this commit will refactor login codes

--- server/controllers/ManagerController.js
+++ server/controllers/ManagerController.js
@@ -6,9 +6,7 @@ import notify from "../helpers/SendEmail";
 import mm from '../models/ManagerModel';
 import dHelper from "../database/dbHelper";
 
-
-class Manager {  
-                
+class Manager {                  
     async signUp(req, res){      
        const  { error } = validators.managerInfo(req.body);
        if(error) {
@@ -74,12 +72,13 @@ class Manager {
         try {
             const {rows} = await dHelper.query('SELECT * FROM managers WHERE email=$1', [req.body.email]);
          if(rows.length === 0){
+             
             return res.status(401).json({
                 status:401,
                 error: 'Invalid Credentials'
             });  
          } else {
-          const passCheck = await bcrypt.compare(req.body.password, req.body.password);
+          const passCheck = await bcrypt.compareSync( req.body.password, rows[0].password);          
           if(!passCheck) {
             return res.status(401).json({
                 status:401,
@@ -87,15 +86,16 @@ class Manager {
             });
         } 
          const user = {
-             email: req.body.email,
-             managerid: req.body.managerid,
-             names:req.body.names,
-             nid: req.body.nid
+             email: rows[0].email,
+             managerid: rows[0].managerid,
+             names: rows[0].names,
+             nid: rows[0].nid,
+             phone: rows[0].phone
          }
          const token = mm.generateToken(user);
             return res.send({
                 success: 200,
-                manager: req.body,
+                manager: user,
                 token:token
             });  
          }            
--- server/test/testConfig/token.js
+++ server/test/testConfig/token.js
@@ -1,6 +1,6 @@
 const data = {
     token: {
-        'x-auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImphenpvbmFoaW1hbmFAZ21haWwuY29tIiwibWFuYWdlcmlkIjozOCwibmFtZXMiOiJOYWhpbWFuYSBKYW52aWVyIiwibmlkIjoiMTE5OTY4MDA4NTI3NzAzNyIsImlhdCI6MTU4MDEwNzcxNSwiZXhwIjoxNTgwMTk0MTE1LCJpc3MiOiJ3d3cuand0LmlvIn0.ZfRdwMmk6rA3NXFb2aEMgAyjEhnJprNo5_unioI6pIA'
+        'x-auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Inlva29oYW1hQGdtYWlsLmNvbSIsIm1hbmFnZXJpZCI6NjgsIm5hbWVzIjoiTmFoaW1hbmEgSmFudmllciIsIm5pZCI6IjExOTk2ODAwODUyNzcxMzAiLCJpYXQiOjE1ODAyMDQyNDIsImV4cCI6MTU4MDI5MDY0MiwiaXNzIjoid3d3Lmp3dC5pbyJ9.n2oStTmksTKVgQQzO9_WJFZU91NYjvXZIqa12kAo7xs'
     }
 };
     
